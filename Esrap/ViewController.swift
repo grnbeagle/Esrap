4247be0649375941aea9aaecb537be7cb7381e36
Amie Kweon <amie.kweon@rd.io>
2015-04-30 17:46:29 -0700

    Initial commit

---
+++ Esrap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Esrap
+//
+//  Created by Amie Kweon on 4/30/15.
+//  Copyright (c) 2015 Rdio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
